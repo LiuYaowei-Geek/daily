b5bde62196fad02d3ca4f224855fea7a8ff523a5
lyw <edison.lyw@gmail.com>
2020-04-21 23:59:10 +0800

    20200421

---
+++ leetCode/src/main/java/com/lyw/leetCode/MedianofTwoSortedArrays.java
@@ -0,0 +1,13 @@
+package com.lyw.leetCode;
+
+public class MedianofTwoSortedArrays {
+
+    private static double solution(int[] nums1, int[] nums2) {
+
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ note/src/main/java/com/lyw/note/java/java.md
@@ -0,0 +1,4 @@
+#JAVA
+1. new出来的对象，基本类型为初始值，String类型为"null"，Object为null
+2. 接口调用时，使用BeanCopy，在对象类型不同时，不能copy
+3. 
