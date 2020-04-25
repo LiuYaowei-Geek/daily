cb1ac23572abb9033d10f5b93dac12901986d432
lyw <edison.lyw@gmail.com>
2020-04-25 21:44:27 +0800

    remove

--- transaction/src/main/java/com/lyw/transaction/statemachine/Handler.java
+++
@@ -1,9 +0,0 @@
-package com.lyw.transaction.statemachine;
-
-/**
- * @author liuyaowei488
- * @date created in 2019-8-26 16:23
- */
-public interface Handler<C extends Context> {
-    void handle(C context, StateMachine stateMachine);
-}
